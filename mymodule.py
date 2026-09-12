7cc4625a80df7d3260f3a5e49234bbda8c61a3da
Siri Atluri <siriatluri2005@gmail.com>
2026-09-12 12:26:38 +0530

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,27 @@
+'''def greetings(name):
+    print("welcome",name)'''
+
+'''a=4
+b=8
+print(a+b)'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+print(a+b)'''
+
+details={"idnos":[10,20,30],
+         "names":["siri", "anu", "sai"],
+         "marks":[60,70,80]}
+
+'''if __name__=="__main__" :
+    a=[10,20,30,40,50]
+    a.append("code")
+    a.extend("code")
+    print(a)'''
+
+'''def dummy():
+    if __name__=="__main__":
+        print("this program is run as script")
+    else:
+        print("this program is run as module")
+dummy()'''     
