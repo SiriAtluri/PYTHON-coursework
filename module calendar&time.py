693411135c7e078382ca8487d903ebcf972d5201
Siri Atluri <siriatluri2005@gmail.com>
2026-09-12 12:25:35 +0530

    Add files via upload

---
+++ module calendar&time.py
@@ -0,0 +1,49 @@
+#calender module
+'''import calendar
+year=2026
+month=8
+print(calendar.month(year,month))'''
+
+#year
+'''import calendar
+year=2027
+print(calender.calendar(year))'''
+
+'''import calendar
+year=int(input("enter the year"))
+print(calendar.calendar(year))'''
+
+'''import calendar
+a=int(input("year"))
+b=int(input("months"))
+print(calendar.month(a,b))'''
+
+#date & Time
+'''from datetime import date
+a=date.today()
+print(a)'''
+
+'''import datetime
+a=datetime.datetime.now()
+print(a)'''
+
+'''import time
+a=time.time()
+print(a)'''#epoch time
+
+'''b=time.localtime(a)
+print(b)'''
+
+'''print(f"today date is{b.tm_mday}-{b.tm_mon}-{b.tm_year}")'''
+'''print(f"time is {b.tm_hour}-{b.tm_min}-{b.tm_sec}")'''
+'''print(f"day is {b.tm_mday}-{b.tm_yday}-{b.tm_isdst}")'''
+
+'''import random
+import time
+for i in range(20):
+    a=random.randint(1000,9999)
+    print(a)
+    time.sleep(2)'''
+
+
+
